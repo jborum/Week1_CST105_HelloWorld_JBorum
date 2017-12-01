a57d74715e6fde83064a19569f01ba7a62298309
Jason Borum <33642393+jborum@users.noreply.github.com>
2017-11-30 17:39:29 -0700

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,13 @@
+/**
+ * This is my own work Jason E. Borum 11/19/2017 Programming Exercise 1.3
+ * CST-105
+ */
+
+public class Introduction {
+
+	// Main method
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
